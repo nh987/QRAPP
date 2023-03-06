a1c35222fe0eb1a101c59b0d051674126e105ca9
Will-Boytinck <48191282+Will-Boytinck@users.noreply.github.com>
2023-03-05 23:35:11 -0700

    Player.java

---
+++ app/src/main/java/com/example/qrapp/Player.java
@@ -0,0 +1,47 @@
+package com.example.qrapp;
+
+public class Player
+{
+    private String username;
+    private String email;
+    private String location;
+    private String phoneNumber;
+
+    public Player(String usrN, String em, String loc, String ph) {
+        this.username = usrN;
+        this.email = em;
+        this.location = loc;
+        this.phoneNumber = ph;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
