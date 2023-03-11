486c811936381dbdd78cb74c92d9060c795361f7
James Laidlaw <78901538+James-Laidlaw@users.noreply.github.com>
2023-03-11 02:39:45 -0700

    MyProfile improvements Adapted myprofile page to work with new userID system added the ability for users to change their name & email added loading indicator (username and email text is "Loading..." until query returns)

--- app/src/main/java/com/example/qrapp/MyProfile.java
+++ app/src/main/java/com/example/qrapp/MyProfile.java
@@ -3,13 +3,11 @@ package com.example.qrapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -21,6 +19,11 @@ public class MyProfile extends AppCompatActivity {
 
     ImageButton backButton;
 
+    String userID;
+
+    String username;
+
+    String email;
     //TODO Stats section
 
     @Override
@@ -35,36 +38,51 @@ public class MyProfile extends AppCompatActivity {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        String deviceID = "DefaultDummyDevice"; //TODO this should be the device ID once we set up registration
-        //query the database for the user with the device ID
-        db.collection("Users").whereEqualTo("DeviceID", deviceID).get().addOnCompleteListener(task -> {
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        // get the user data from the database
+        db.collection("Users").document(userID).get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                //check that the query returned a result
-                if(task.getResult().getDocuments().size() == 0){
-                    //raise an error toast and return to the previous activity
-                    Toast toast = Toast.makeText(getApplicationContext(), "Error: No user found", Toast.LENGTH_SHORT);
-                    toast.show();
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    //set the attributes
+                    username = document.getString("username");
+                    email = document.getString("email");
+
+                    //set the text views
+                    usernameText.setText(username);
+                    emailText.setText(email);
+                } else {
+                    Toast.makeText(MyProfile.this, "Error getting user data", Toast.LENGTH_SHORT).show();
                     finish();
                     return;
                 }
-                //get the first document in the query
-                DocumentSnapshot document = task.getResult().getDocuments().get(0);
-                //set the text views to the user's info
-                usernameText.setText(document.getString("Username"));
-                emailText.setText(document.getString("Email"));
             } else {
-                //raise an error toast and return to the previous activity
-                Toast toast = Toast.makeText(getApplicationContext(), "Error: Could not connect to database", Toast.LENGTH_SHORT);
-                toast.show();
+                Toast.makeText(MyProfile.this, "Error getting user data", Toast.LENGTH_SHORT).show();
                 finish();
                 return;
             }
         });
 
-        //close activity when back button is pressed
-        backButton.setOnClickListener(v -> {
-            finish();
+        //watch for the user updating their username
+        usernameText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus && !usernameText.getText().toString().equals(username)){
+                //update the username in the database
+                db.collection("Users").document(userID).update("username", usernameText.getText().toString());
+            }
         });
+
+        // watch for the user updating their email
+        emailText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus && !emailText.getText().toString().equals(email)){
+                //update the email in the database
+                db.collection("Users").document(userID).update("email", emailText.getText().toString());
+            }
+        });
+
+        //close activity when back button is pressed
+        backButton.setOnClickListener(v -> finish());
 
     }
 
