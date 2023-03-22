38841c455a45f70bbdc6c07a725f963498128544
Byvalvin <usertakeny@gmail.com>
2023-03-22 09:01:55 -0600

    Seeing, not believeing
    Was able to get the list of players to show for the score ranking.
    
    It is the wrong output however. I am struggling to query the db properly to get the top 10. The worst part is, it was working perfectly at one point. Then all of a sudden nothing makes sense.
    
    I will take another look later. Might be an accident delete of a line of code or maybe I can get someone else to do the querying.

--- app/src/main/java/com/example/qrapp/RankTriple.java
+++ app/src/main/java/com/example/qrapp/RankTriple.java
@@ -3,11 +3,13 @@ package com.example.qrapp;
 public class RankTriple {
 
     public String PlayerID="";
-    public QRCode QRc=null;
+    public String QRcFace = "";
+    public Long QRcPoints = 0L;
 
-    public RankTriple(String playerID, QRCode qrCode){
+    public RankTriple(String playerID, String qrcFace, Long qrcPoints){
         this.PlayerID=playerID;
-        this.QRc=qrCode;
+        this.QRcFace = qrcFace;
+        this.QRcPoints = qrcPoints;
     }
 
 }
