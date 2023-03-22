2cc609b27f821ac38e4bd900f32c460afadeaf64
“Evan-Maloney” <ejmalone@ualberta.ca>
2023-03-21 22:50:39 -0600

    Update comments and javadocs
    :x
    
    '

--- app/src/main/java/com/example/qrapp/Comment.java
+++ app/src/main/java/com/example/qrapp/Comment.java
@@ -5,38 +5,62 @@ public class Comment {
     private String authorUsername;
     private String commentText;
 
+    /**
+     *`Constructor for the Comment class
+     * @param authorId The id of the user who wrote the comment
+     * @param authorUsername The username of the user who wrote the comment
+     * @param commentText The text of the comment
+     */
     public Comment(String authorId, String authorUsername, String commentText) {
         this.authorId = authorId;
         this.authorUsername = authorUsername;
         this.commentText = commentText;
     }
 
-    // Add the getter for the authorId field
+    /**
+     * Returns the id of the user who wrote the comment
+     * @return The id of the user who wrote the comment
+     */
     public String getAuthorId() {
         return authorId;
     }
 
-    // Add the setter for the authorId field
+    /**
+     * Sets the id of the user who wrote the comment
+     * @param authorId
+     */
     public void setAuthorId(String authorId) {
         this.authorId = authorId;
     }
 
-    // Add the getter for the authorUsername field
+    /**
+     * Returns the username of the user who wrote the comment
+     * @return The username of the user who wrote the comment
+     */
     public String getAuthorUsername() {
         return authorUsername;
     }
 
-    // Add the setter for the authorUsername field
+    /**
+     * Sets the username of the user who wrote the comment
+     * @param authorUsername The username of the user who wrote the comment
+     */
     public void setAuthorUsername(String authorUsername) {
         this.authorUsername = authorUsername;
     }
 
-    // Add the getter for the commentText field
+    /**
+     * Returns the text of the comment
+     * @return The text of the comment
+     */
     public String getCommentText() {
         return commentText;
     }
 
-    // Add the setter for the commentText field
+    /**
+     * Sets the text of the comment
+     * @param commentText The text of the comment
+     */
     public void setCommentText(String commentText) {
         this.commentText = commentText;
     }
