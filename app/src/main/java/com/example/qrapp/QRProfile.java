7a3f1cd9aede8c14a7da9be467f95b01a8ed145c
mh575 <101076328+mh575@users.noreply.github.com>
2023-03-25 07:11:02 -0600

    removed button import

--- app/src/main/java/com/example/qrapp/QRProfile.java
+++ app/src/main/java/com/example/qrapp/QRProfile.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import com.example.qrapp.Comment;
 
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
